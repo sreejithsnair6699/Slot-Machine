eeed1d09a4dce6f986868c14f1afa09ff3bac82b
SJ\Sreejith <sreejithsanir6699@gmail.com>
2018-10-12 22:18:45 -0400

    Setting Up createJS support

---
+++ Scripts/core/game.js
@@ -0,0 +1,8 @@
+(function () {
+    function Start() {
+    }
+    function Update() {
+    }
+    window.addEventListener("load", Start);
+})();
+//# sourceMappingURL=game.js.map
---
+++ Scripts/core/game.ts
@@ -0,0 +1,13 @@
+(function(){
+    
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    window.addEventListener("load", Start);
+
+})();
